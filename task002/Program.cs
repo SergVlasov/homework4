d3ce9969a01706fac1a4648f9abf21b71daaf1cc
Serg Vlasov <stalkerloko2015@gmail.com>
2022-11-08 01:04:49 +0300

    Editing files

---
+++ task002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11 | 82 -> 10 | 9012 -> 12
+
+Console.WriteLine("Hello. Enter a number:");
+int num = Convert.ToInt32(Console.ReadLine());
+int i =1;
+int sum = 0;
+while (num != 0)
+{
+    i = num % 10;
+    sum = sum + i;
+    num = num / 10;
+}
+Console.WriteLine(sum);

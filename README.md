17338ed362cf20cffbeda29c27972ca3a8a66806
Serg Vlasov <stalkerloko2015@gmail.com>
2022-11-08 02:07:48 +0300

    Editing files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # homework4
+
+## Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в целую степень B.
+3, 5 -> 243 (3⁵);  2, 4 -> 16     без использования math.pow
+
+[Блок-схема](homework\homework4\task001\block diagram01.png) | [Код](homework\homework4\task001\Program.cs)
+
+## Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11 | 82 -> 10 | 9012 -> 12
+
+[Блок-схема](homework\homework4\task002\block diagram02.png) | [Код](homework\homework4\task002\Program.cs)
+
+## Напишите программу, которая задаёт массив из N элементов и выводит их на экран. N = 5, получаем массив [1, 2, 5, 7, 19]  N = 3, получаем массив [6, 1, 33]
+
+[Блок-схема](homework\homework4\task002\block diagram02.png) | [Код](homework\homework4\task003\Program.cs)
---
+++ task003/Program.cs
@@ -0,0 +1,35 @@
+﻿// *Напишите программу, которая задаёт массив из N элементов и выводит их на экран. N = 5, получаем массив [1, 2, 5, 7, 19] 
+// N = 3, получаем массив [6, 1, 33]
+
+Console.WriteLine("Hi. Please enter a number from 1: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while(num < 1)
+{
+    Console.WriteLine("Error. Enter a number from 1: ");
+    num = Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int[] array)
+{
+    int i=0;
+    int count = array.Length;
+    while (i < array.Length)
+    {
+        array[i] = new Random().Next(1, num);
+        i++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int i = 0;
+    int count = array.Length;
+    while (i < array.Length)
+    {
+        Console.Write(array[i]);
+        i++;
+    }
+}
+int[] array = new int [num];
+FillArray(array);
+PrintArray(array);
